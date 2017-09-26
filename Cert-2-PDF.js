206563fead2608a614a81736144297f350efa1fd
cmopbeatz <31453162+cmopbeatz@users.noreply.github.com>
2017-09-26 05:14:11 -0700

    x

--- Cert-2-PDF.js
+++ Cert-2-PDF.js
@@ -1,20 +1,65 @@
 // Cert-to-PDF
 
-function Cert2PDF(){
-html2canvas(document.getElementById('CertForm'), {
+
+function certScreen(){
+	html2canvas(document.getElementById('CertForm'),{
+		onrendered:function(cvs){
+			
+			var coaImg = new Image();
+			
+			var cvs = document.createElement('canvas');
+			cvs.width = 800;
+			cvs.height = 800;
+			
+			var dl = document.getElementById('dlPDF');
+			dl.href = cvs.toDataURL('image/jpg');
+			dl.download = 'cmop-L00.jpg';
+			dl.click;
+			
+			var cvsTxt = cvs.getContext('2d');
+			cvsTxt.drawImage(coaImg,0,0);
 	
-	onrendered: function(canvas){
-		var img = canvas.toDataURL('image/jpeg');
-		
-		var doc = new jsPDF();
-		doc.addImage(img, 'JPEG',20,20);
-		doc.save('coaL00.pdf');
+			coaImg.src = URL.createObjectURL(new blob);
+					
+			window.open(dl.href);
 	}
-});
+	
+	});
 }
 
+//function Cert2PDF(){
+//html2canvas(document.getElementById('CertForm'), {
+	
+	//onrendered: function(canvas){
+		//var img = canvas.toDataURL('image/jpeg');
+		
+		//var doc = new jsPDF();
+		//doc.addImage(img, 'JPEG',20,20);
+		//doc.save('coaL00.pdf');
+	//}
+//});
+//}
 
 
+		//$(function() { 
+
+   // $("#dlPDF").click(function() { 
+
+     //   html2canvas($("#CertForm"), {
+
+       //     onrendered: function(canvas) {
+
+         //     return Canvas2Image.saveAsPNG(canvas); 
+
+                
+
+      //      }
+
+    //    });
+
+  //  });
+
+//}); 
 //var pdfCanvas = document.getElementById('coaPreview');
 
 
