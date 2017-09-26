eda9de9b981a2fe64a1671dce73cc68f63645978
cmopbeatz <31453162+cmopbeatz@users.noreply.github.com>
2017-09-26 02:42:29 -0700

    cert-2pdf

--- Cert-2-PDF.js
+++ Cert-2-PDF.js
@@ -1,16 +1,17 @@
 // Cert-to-PDF
 
 function Cert2PDF(){
-html2canvas(document.body, {
+html2canvas(document.getElementById('CertForm'), {
 	
 	onrendered: function(canvas){
-		var img = canvas.toDataURL('image/png');
+		var img = canvas.toDataURL('image/jpeg');
+		
 		var doc = new jsPDF();
 		doc.addImage(img, 'JPEG',20,20);
 		doc.save('coaL00.pdf');
 	}
 });
-
+}
 
 
 
@@ -18,23 +19,24 @@ html2canvas(document.body, {
 
 
 	
-//	$('#dlPDF').click(function() {
-  //var options = {};
-//  var pdf = new jsPDF();
+	//$('#dlPDF').click(function() {
+//var options = {};
+  //var pdf = new jsPDF();
 
-  //pdf.addHTML($("#CertForm"), 15, 15, options, function() {
+//pdf.addHTML($("#CertForm"), 15, 15, options, function() {
     //pdf.save('pageContent.pdf');
-//  });
+  //});
 //});
 
 
 //var element = document.querySelectorAll("CertForm")[0];
 //options = {
-  //  onrendered: function(canvas) {
+    //onrendered: function(canvas) {
        //$('CertForm').append(canvas);
-    //    var imgstring = canvas.toDataURL();
-      //  $('.result').attr('src', imgstring);
-   // }};
+        //var imgstring = canvas.toDataURL();
+        //$('.result').attr('src', imgstring);
+    //}};
 //html2canvas(element, options);
 
-}
+
+//}
