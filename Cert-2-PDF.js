f1d13977f7e398187dcf06a11503360f38dfffed
cmopbeatz <31453162+cmopbeatz@users.noreply.github.com>
2017-09-26 13:51:07 -0700

    verfy-chng

--- Cert-2-PDF.js
+++ Cert-2-PDF.js
@@ -2,30 +2,35 @@
 
 
 function certScreen(){
-	html2canvas(document.getElementById('CertForm'),{
-		onrendered:function(cvs){
-			
-			var coaImg = new Image();
-			
-			var cvs = document.createElement('canvas');
-			cvs.width = 800;
-			cvs.height = 800;
-			
-			var dl = document.getElementById('dlPDF');
-			dl.href = cvs.toDataURL('image/jpg');
-			dl.download = 'cmop-L00.jpg';
-			dl.click;
-			
-			var cvsTxt = cvs.getContext('2d');
-			cvsTxt.drawImage(coaImg,0,0);
 	
-			coaImg.src = URL.createObjectURL(new blob);
+	//GrabzIt("ZjczYWY0MDVlZWMzNDMwNDlhMDRlZTg2YjNlZmJhNTA=").ConvertURL("https://cmopbeatz.github.io/cmop-beatz/verify.html",
+//{"target": "#CertForm", "bheight": -1, "height": -1, "width": -1}).Create();
+	
+	
+	//html2canvas(document.getElementById('CertForm'),{
+		//onrendered:function(cvs){
+	//		
+//			//var coaImg = new Image();
+			
+			//var cvs = document.createElement('canvas');
+			//cvs.width = 800;
+			//cvs.height = 800;
+			
+			//var dl = document.getElementById('dlPDF');
+			//dl.href = cvs.toDataURL('image/jpg');
+			//dl.download = 'cmop-L00.jpg';
+			//dl.click;
+			
+			//var cvsTxt = cvs.getContext('2d');
+			//cvsTxt.drawImage(coaImg,0,0);
+	
+			//coaImg.src = URL.createObjectURL();
 					
-			window.open(dl.href);
-	}
+			//window.open(dl.href);
+	//}
 	
-	});
-}
+	//});
+//}
 
 //function Cert2PDF(){
 //html2canvas(document.getElementById('CertForm'), {
@@ -70,18 +75,18 @@ function certScreen(){
 
 //pdf.addHTML($("#CertForm"), 15, 15, options, function() {
     //pdf.save('pageContent.pdf');
-  //});
+//  });
 //});
 
 
-//var element = document.querySelectorAll("CertForm")[0];
-//options = {
-    //onrendered: function(canvas) {
-       //$('CertForm').append(canvas);
-        //var imgstring = canvas.toDataURL();
-        //$('.result').attr('src', imgstring);
-    //}};
-//html2canvas(element, options);
+var element = document.querySelectorAll("CertForm")[0];
+options = {
+    onrendered: function(canvas) {
+       $('CertForm').append(canvas);
+        var imgstring = canvas.toDataURL();
+        $('.result').attr('src', imgstring);
+    }};
+html2canvas(element, options);
 
 
-//}
+}
