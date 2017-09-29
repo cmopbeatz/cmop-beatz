0c0ea7098a8d030d73d321a18440b74a46ef5a25
cmopbeatz <31453162+cmopbeatz@users.noreply.github.com>
2017-09-28 18:18:51 -0700

    chng-618pst

--- Cert-2-PDF.js
+++ Cert-2-PDF.js
@@ -1,10 +1,20 @@
 
 // Cert-to-PDF
+var certificate, pdfDL, previewPDF;
+
+certificate = document.getElementById('CertForm');
+pdfDL = document.getElementById('dlPDF');
+previewPDF = document.getElementById('coaPreview');
 
 
 function certScreen(){
-	
-	
+	html2canvas(certificate, {
+		onrendered:function(canvas){
+			previewPDF.innerHTML = "";
+			previewPDF.appendChild(canvas);
+			
+		}
+	});
 }
 
 
