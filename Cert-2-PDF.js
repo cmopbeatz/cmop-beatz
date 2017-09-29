c32c333f9cb3e688e3228b7679feaa5247bd673b
cmopbeatz <31453162+cmopbeatz@users.noreply.github.com>
2017-09-28 22:24:01 -0700

    chng-1023pst

--- Cert-2-PDF.js
+++ Cert-2-PDF.js
@@ -8,13 +8,35 @@ previewPDF = document.getElementById('coaPreview');
 
 
 function certScreen(){
-	html2canvas(certificate, {
-		onrendered:function(canvas){
-			previewPDF.innerHTML = "";
-			previewPDF.appendChild(canvas);
-			
-		}
-	});
+	
+	
+	var getImg = function(url, callback){
+		
+		img = new Image();
+		img.onload = function(){
+			callback(img);
+		};
+		img.src = url;
+	}
+	
+	
+	
+	
+	var margin = {top:20,left:5,right:5,bottom:0};
+	
+	
+	
+	var doc = new jsPDF('landscape');
+	
+	doc.fromHTML($('#CertForm').get(0),20,20,{
+		'width':850,'height':450}, function (renderCall){ doc.save("cmopL00.pdf");
+		
+	}, margin);
 }
+		
+	  
+	
+
+
 
 
