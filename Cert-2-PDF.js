2e4e0513c106baac4d4d6287f163b4305f0cc939
cmopbeatz <31453162+cmopbeatz@users.noreply.github.com>
2017-09-30 12:04:13 -0700

    chng-1204pst

--- Cert-2-PDF.js
+++ Cert-2-PDF.js
@@ -8,14 +8,14 @@ var certificate, pdfDL, previewPDF;
 function certScreen(){
 	
 	
- certificate = document.getElementById('CertForm');
- pdfDL = document.getElementById('dlPDF');
+var certificate = document.getElementById('CertForm');
+var pdfDL = document.getElementById('dlPDF');
 previewPDF = document.getElementById('coaPreview');	
 	
 	html2canvas(document.getElementById('CertForm'),{
 		onrendered: function(canvas){
 		const imgDataURL = canvas.toDataURL('image/png')
-	var doc = new jsPDF('landscape','mm','a4');
+	var doc = new jsPDF('landscape','px','a4');
 	doc.addImage(imgDataURL, 'PNG',20,20);
 	doc.save('cmopL00.pdf');
 		}
@@ -29,16 +29,16 @@ previewPDF = document.getElementById('coaPreview');
 		
 		//img = new Image();
 		//img.onload = function(){
-		//	callback(img);
+			//callback(img);
 		//};
-		///img.src = url;
-	//}//
+		//img.src = url;
+	//}
 	
 	//var margin = {top:20,left:5,right:5,bottom:0};
 	
 	
 	
-//var doc = new jsPDF('landscape','mm','a4');
+//var doc = new jsPDF('landscape','px','a4');
 	//var pdfW;
 //	var pdfH; 
 	
@@ -46,11 +46,11 @@ previewPDF = document.getElementById('coaPreview');
 //	pdfH = document.getElementById('CertForm').style.height;
 	
 	
-//	doc.addImage(img, 'PNG',10,10);
-	//doc.fromHTML($('#CertForm').get(0),20,20,{
-//		'width':pdfW,'height':pdfH}, function (){ doc.save("cmopL00.pdf");
+	//doc.addImage(imgDataURL, 'PNG',10,10);
+//	doc.fromHTML($('#CertForm').get(0),20,20,{
+	//	'width':pdfW,'height':pdfH}, function (){ doc.save("cmopL00.pdf");
 		
-	//}, margin);
+//	}, margin);
 			
 }
 		
