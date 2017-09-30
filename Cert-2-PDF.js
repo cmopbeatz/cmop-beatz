b1d36733aaa67989dde67fba4311918f961aaf75
cmopbeatz <31453162+cmopbeatz@users.noreply.github.com>
2017-09-30 11:42:45 -0700

    chng-1142pst

--- Cert-2-PDF.js
+++ Cert-2-PDF.js
@@ -12,45 +12,45 @@ var certificate = document.getElementById('CertForm');
 var pdfDL = document.getElementById('dlPDF');
 previewPDF = document.getElementById('coaPreview');	
 	
-	html2canvas(document.getElementById('CertForm'),{
-		onrendered: function(canvas){
-		const imgDataURL = canvas.toDataURL('image/png')
-	var doc = new jsPDF('landscape','px','a4');
-	doc.addImage(imgDataURL, 'JPEG',20,20);
-	doc.save('cmopL00.pdf');
-		}
-	});
+	//html2canvas(document.getElementById('CertForm'),{
+		//onrendered: function(canvas){
+		//const imgDataURL = canvas.toDataURL('image/png')
+	//var doc = new jsPDF('landscape','px','a4');
+	//doc.addImage(imgDataURL, 'PNG',20,20);
+	//doc.save('cmopL00.pdf');
+		//}
+	//});
 		
 	
 	
 	
 	
-	//var getImg = function(url, callback){
+	var getImg = function(url, callback){
 		
-		//img = new Image();
-		//img.onload = function(){
-			//callback(img);
-		//};
-		//img.src = url;
-	//}
+		img = new Image();
+		img.onload = function(){
+			callback(img);
+		};
+		img.src = url;
+	}
 	
-	//var margin = {top:20,left:5,right:5,bottom:0};
-	
-	
-	
-//	var doc = new jsPDF('landscape','px','a4');
-	//var pdfW;
-//	var pdfH; 
-	
-	//pdfW = document.getElementById('CertForm').style.width;
-//		pdfH = document.getElementById('CertForm').style.height;
+	var margin = {top:20,left:5,right:5,bottom:0};
 	
 	
-	//doc.addImage(img, 'PNG',10,10);
-	//doc.fromHTML($('#CertForm').get(0),20,20,{
-		//'width':pdfW,'height':pdfH}, function (){ doc.save//("cmopL00.pdf");
+	
+var doc = new jsPDF('landscape','px','a4');
+	var pdfW;
+	var pdfH; 
+	
+	pdfW = document.getElementById('CertForm').style.width;
+	pdfH = document.getElementById('CertForm').style.height;
+	
+	
+	doc.addImage(img, 'PNG',10,10);
+	doc.fromHTML($('#CertForm').get(0),20,20,{
+		'width':pdfW,'height':pdfH}, function (){ doc.save("cmopL00.pdf");
 		
-	//}, margin);
+	}, margin);
 			
 }
 		
