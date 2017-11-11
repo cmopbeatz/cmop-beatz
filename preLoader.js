63c3f6ab8a2d9bb549a407f1a44897f18a4f1f1a
cmopbeatz <31453162+cmopbeatz@users.noreply.github.com>
2017-11-10 17:07:12 -0800

    chng-507pst

--- preLoader.js
+++ preLoader.js
@@ -14,10 +14,12 @@ $('#audio_player').css('display','none');
 $(document).ready(function () {
 	$('.container').css('display','none');
 	$('#audio_player').css('display','none');
-  
+  	$('#Enter-page').attr("disabled",true);
+		
+		
     setTimeout(function () {
        
-		
+		$('#Enter-page').attr("disabled",false);
 		$('#Enter-page').text("enter cmopbeatz.com");
 		$('#loader-graphic').css('display','none');
     }, 15000);
